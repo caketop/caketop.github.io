3457b00b3ddebfe3ff275075a9275c52989cec35
Jordan Webb <jordan@caketop.app>
2022-09-02 12:04:02 -0500

    Add astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 
+import compress from "astro-compress";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://caketop.app/",
-  integrations: [tailwind(), image()],
+  integrations: [tailwind(), image(), compress()]
 });
