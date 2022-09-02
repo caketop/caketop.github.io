05cc90d065c45e9e952b6ecb235c80a39564e690
Jordan Webb <jordan@caketop.app>
2022-09-02 11:59:47 -0500

    Switch to @astrojs/image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import image from "@astrojs/image";
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://caketop.github.io/",
-  integrations: [tailwind()],
-  vite: {
-    ssr: {
-      external: ["@11ty/eleventy-img", "svgo"],
-    },
-  },
+  site: "https://caketop.app/",
+  integrations: [tailwind(), image()],
 });
